ff6ca15f64b565916877ca924528d82a01ced839
Daniel <danielk@pop-os.localdomain>
2020-12-21 20:41:38 +0200

    First commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop() > 10) {
+            $('.navbar').addClass('active');
+        } else {
+            $('.navbar').removeClass('active');
+        }
+    });
+});
